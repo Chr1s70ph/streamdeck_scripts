a5691035eddb3742c3b8a2a0e077bf4890b120cc
Christoph <christoph.peter.bluem@gmail.com>
2025-02-17 18:21:10 +0100

    adds sleeping monitors

--- sleep.sh
+++ sleep.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+# turn off main display
+m1ddc -v display 1 set standby 4
+
+# turn off secondary display
+m1ddc -v display 2 set standby 4
+
 # Sets the mac to sleep
 pmset displaysleepnow
