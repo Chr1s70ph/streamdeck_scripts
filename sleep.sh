d52f982793b8b5b00f4b6e28c41f691310040405
Christoph <christoph.peter.bluem@gmail.com>
2025-02-18 08:11:03 +0100

    oh streamdeck, how shall I please you more? 🎀

--- sleep.sh
+++ sleep.sh
@@ -1,11 +1,7 @@
-#!/bin/sh
+#!/bin/env bash
 
 # turn off secondary display
-m1ddc -v display 2 set standby 4
-
-# have to sleep, because ddc takes some time
-# and I don't want to put more energy into making this better
-sleep 2
+/opt/homebrew/bin/m1ddc -v display 2 set standby 4
 
 # turn off main display
-m1ddc -v display 1 set standby 4
+/opt/homebrew/bin/m1ddc -v display 1 set standby 4
